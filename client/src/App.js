8138b003df1754167686b82a0d9dfc7d0b189410
Farhana <farhanafatema@hotmail.com>
2020-10-14 16:00:18 +0100

    trying to deploy this brnach on heruko

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyle";
 import { theme } from "./styles/Theme";
-// import "./App.css";
-// import { getMessage } from "./service";
-// import logo from "./logo.svg";
+
 
 const App = () => {
 	return (
--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			favicon: "./client/src/favicon.ico",
+			// favicon: "./client/src/favicon.ico",
 			template: "./client/src/index.html",
 		}),
 	],
